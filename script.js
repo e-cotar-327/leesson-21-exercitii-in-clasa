2a1b254a969c08c7da99a035f23055cf59c26996
Elena Cotar <e_cotar@yahoo.com>
2024-08-26 13:05:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+// Exercitiul 1
+const country = 'Republic of Moldova';
+const continent = 'Europa';
+let population = 2.539;
+//console.log(country, continent, population);
+
+// Exercitiul 2
+const isIsland = false;
+let language;
+
+// Exercitiul 3
+//console.log(typeof isIsland);
+//console.log(typeof population);
+//console.log(typeof country);
+//console.log(typeof language);
+
+// Exercitiul 4
+language = 'romanian';
+
+// Exercitiul 5
+let halfPopulation = population / 2;
+//console.log(halfPopulation);
+let quarterPopulation = halfPopulation / 2;
+//console.log(quarterPopulation);
+
+// Exercitiul 6
+let plusMlnPopulation = ++population;
+//console.log(plusMlnPopulation);
+
+// Exercitiul 7
+let populationFinland = 6;
+//console.log(population > populationFinland);
+
+// Exercitiul 8
+let averagePopulation = 33;
+//console.log(population < averagePopulation);
+
+// Exercitiul 9
+let description = country + " este in " + continent + " si cei " + population + " milioane de cetateni ale ei vorbesc " + language + ".";
+console.log(description);
